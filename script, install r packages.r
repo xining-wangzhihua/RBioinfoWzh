08e43d545c0ab679a6d89b447e9bbf1eaa7c8861
ywd5 <42758126+ywd5@users.noreply.github.com>
2020-03-02 20:39:25 -0800

    Add files via upload

--- script, install r packages.r
+++ script, install r packages.r
@@ -1,16 +1,44 @@
-#last edited at 20191211
+#last edited at 20200303
 
+
+message("before installing r packages, make sure you have installed Rtools and",
+        "jre (java runtime environment) manually, and set path to Rtools in r.")
+#
+#try this code to set the path to Rtools:
+#Sys.setenv(BINPREF="D:/softwares/Rtools/mingw_$(WIN)/bin/")
+#
 update.packages()
 
-list_1=c("ape","dplyr","fastmatch","ggplot2","gtools","magrittr","questionr",
-         "readr","rlang","shiny","stringi","stringr","tibble","XML")
-list_bioconductor=c("Biostrings","msa","S4Vectors","sangerseqR")
-list_2=c("curl","data.table","devtools","digest","fastmap","plyr","R.oo","R6",
-         "rlist","rncl","RNeXML","phylobase","seqinr","tidyr","utf8","vctrs","xml2")
 
-install.packages(pkgs=list_1)
-install.packages(pkgs="BiocManager")
-BiocManager::install(pkgs=list_bioconductor)
-if(FALSE){install.packages(pkgs=list_2);}
+l.common=c("devtools","ggplot2","gtools","Hmisc","magrittr","questionr",
+           "R.methodsS3","R.oo","R6","Rcpp","R.utils","readxl","rlang","rJava","shiny",
+           "stringr","tibble","xml2")
+#Hmisc may be installed only for `%nin%`
+l.rare=c("curl","data.table","digest","dplyr","fastmap","fastmatch","gdata",
+         "measurements","OSMscale","plyr","RCurl","readr","rlist","rncl",
+         "RNeXML","stringi","tidyr","utf8","vctrs","XiMpLe","xlsx","XML")
+l.bioconductor=c("Biobase","BiocGenerics","IRanges","GenomicRanges","S4Vectors",
+                 "XVector","Biostrings","sangerseqR","msa")
+l.phylo.and.bioinfo=c("ape","haplo.stats","phylobase","seqinr")#doesn't include packages in bioconductor
+l.geography=c("mapdata","mapproj","maps","maptools","OpenStreetMap","rgeos","sf")
 
-rm(list_1,list_2,list_bioconductor)
+
+l.ans=c(l.common, l.phylo.and.bioinfo, l.geography)
+l.ans=setdiff(x=l.ans,y=rownames(installed.packages()))
+install.packages(pkgs=l.ans)
+rm(l.ans)
+#
+if(!require(BiocManager)){
+  install.packages(pkgs="BiocManager")
+}
+l.ans=setdiff(x=l.bioconductor,y=rownames(installed.packages()))
+if(length(l.ans)>0){BiocManager::install(pkgs=l.ans);}
+rm(l.ans)
+#
+if(FALSE){
+  l.rare=setdiff(x=l.rare,y=rownames(installed.packages()))
+  install.packages(pkgs=l.rare)
+}
+
+
+rm(l.common, l.rare, l.bioconductor, l.phylo.and.bioinfo, l.geography)
