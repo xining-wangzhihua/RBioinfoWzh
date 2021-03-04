ce112ea10a4bfe332f18519c2799ecf4e1b68ae0
ywd5 <42758126+ywd5@users.noreply.github.com>
2021-03-04 16:31:05 +0700

    Add files via upload

--- as.DNAStringSet.DNAbin().r
+++ as.DNAStringSet.DNAbin().r
@@ -1,32 +1,39 @@
-#last edited at 20200102
+#a function to convert DNAbin object in ape to DNAStringSet object in Biostrings
 
-#a function to convert ape::DNAbin object to Biostrings::DNAStringSet object
-
-library(Biostrings)
-library(ape)
-library(magrittr)
-
-as.DNAStringSet.DNAbin <- function(dnabin){
-  #>>>control the input of dnabin begin>>>
-  if(!is(object=dnabin,class2="DNAbin")){stop("dnabin must be an DNAbin object defined by ape.");}
-  if(is.matrix(dnabin)){dnabin=as.list(dnabin);}
-  if(!is.list(dnabin)){stop("dnabin must be in list or matrix form, but not vector or other form.")}
-  if(length(dnabin)==0){stop("dnabin must contain at least 1 sequence.")}
-  #<<<control the input of dnabin end<<<
-  #>>>main manipulation begin>>>
-  l=length(dnabin)
-  result=DNAStringSet("") %>% rep(times=l) %>% setNames(nm=labels(dnabin))
-  for(i in 1:l)if(anyNA(as.character(dnabin[i])[[1]])){stop("NO. ",i,"-th sequence in dnabin contain NA.")}
-  for(i in 1:l){result[i]=as.character(dnabin[i]) %>% .[[1]] %>% paste0(collapse="")}
-  #<<<main manipulation end<<<
-  rm(dnabin,l,i)
-  return(result)
+#get dependency
+if(FALSE){
+  library(magrittr)
+  library(Biostrings); library(ape);
 }
 
+as.DNAStringSet.DNAbin=function(x){
+  #control the input of x-------------------------------------------------------
+  if(!is(object=x, class2="DNAbin")){stop("x must be a DNAbin object defined in ape.");}
+  if(is.matrix(x)){x %<>% as.list();}
+  if(!is.list(x)){stop("x must be in list or matrix form, but not vector or other forms");}
+  if(length(x) == 0){stop("length(x) can't 0");}
+  #check if DNAbin contain invalid characters for DNAStringSet------------------
+  lapply(X=x, FUN=as.character) %>% lapply(FUN=anyNA) %>%
+    unlist() %>% base::which() %>% paste0(collapse=" ") %>%
+    {if(. != ""){stop("the following sequences contain NA: ", .)};}
+  lapply(X=x, FUN=as.character) %>% lapply(FUN=function(datum){any(datum == "?")}) %>%
+    unlist() %>% base::which() %>% paste0(collapse=" ") %>%
+    {if(. != ""){stop("the following sequences contain \"?\": ", .)};}
+  #main manipulation------------------------------------------------------------
+  no_names <- list(before=is.null(labels(x)), after=FALSE)
+  x %<>% base::as.character() %>% lapply(FUN=paste0, collapse="") %>%
+    unlist() %>% base::toupper() %>% DNAStringSet()
+  no_names$after <- is.null(names(x))
+  if(no_names$before != no_names$after){stop("sequence names bug");}
+  remove(no_names)
+  #return x---------------------------------------------------------------------
+  return(x)
+}
+
+#here are the test codes
 if(FALSE){
-  #here are the test codes
-  dnabin=list(c("a","c","g","t","M","r"),"") %>% as.DNAbin()
-  dnabin=list(c("a","c","g","t","M","r"),character()) %>% as.DNAbin()
-  dnabin=list(c("a","c","g","t","M","r"),character()) %>% as.DNAbin() %>% setNames(nm=c("x","y"))
-  as.DNAStringSet(dnabin)
+  x <- list(c("a","c","g","t","M","r"), "") %>% as.DNAbin()
+  x <- list(c("a","c","g","t","M","r"), character()) %>% as.DNAbin()
+  x <- list(c("a","c","g","t","M","r"), character()) %>% as.DNAbin() %>% setNames(nm=c("x","y"))
+  as.DNAStringSet.DNAbin(x)
 }
